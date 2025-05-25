c1dcf76b4fca72ff8e7b908bf65449b2ded7c3ef
Cassandra Tolbert <maecass20@gmail.com>
2025-05-25 12:39:28 -0700

    inital commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,5 @@
+const CompanionsLibrary = () => {
+	return <div>Comapanions</div>;
+};
+
+export default CompanionsLibrary;
---
+++ constants/index.ts
@@ -0,0 +1,73 @@
+export const subjects = [
+	'maths',
+	'language',
+	'science',
+	'history',
+	'coding',
+	'economics',
+];
+
+export const subjectsColors = {
+	science: '#A855F7',
+	maths: '#FCCC41',
+	language: '#FF7847',
+	coding: '#39BCF9',
+	history: '#D7E8FC',
+	economics: '#FF97D3',
+};
+
+export const voices = {
+	male: { casual: '2BJW5coyhAzSr8STdHbE', formal: 'c6SfcYrb2t09NHXiT80T' },
+	female: { casual: 'ZIlrSGI4jZqobxRKprJz', formal: 'sarah' },
+};
+
+export const recentSessions = [
+	{
+		id: '1',
+		subject: 'science',
+		name: 'Neura the Brainy Explorer',
+		topic: 'Neural Network of the Brain',
+		duration: 45,
+		color: '#A855F7',
+	},
+	{
+		id: '2',
+		subject: 'maths',
+		name: 'Countsy the Number Wizard',
+		topic: 'Derivatives & Integrals',
+		duration: 30,
+		color: '#FCCC41',
+	},
+	{
+		id: '3',
+		subject: 'language',
+		name: 'Verba the Vocabulary Builder',
+		topic: 'English Literature',
+		duration: 30,
+		color: '#FF7847',
+	},
+	{
+		id: '4',
+		subject: 'coding',
+		name: 'Codey the Logic Hacker',
+		topic: 'Intro to If-Else Statements',
+		duration: 45,
+		color: '#39BCF9',
+	},
+	{
+		id: '5',
+		subject: 'history',
+		name: 'Memo, the Memory Keeper',
+		topic: 'World Wars: Causes & Consequences',
+		duration: 15,
+		color: '#D7E8FC',
+	},
+	{
+		id: '6',
+		subject: 'economics',
+		name: 'The Market Maestro',
+		topic: 'The Basics of Supply & Demand',
+		duration: 10,
+		color: '#FF97D3',
+	},
+];
